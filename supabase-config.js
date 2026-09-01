faedf160f865b59b54dad2c9f816a99da597f657
bfmounir <bfmounir@gmail.com>
2026-09-01 13:14:12 +0200

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = 
+"https://vgbgvdbssiuhzeepexll.supabase.co";
+
+const SUPABASE_KEY = 
+"sb_publishable_8_ACYxPHx8ZYi5oA61x9rQ_IwNQossu";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
