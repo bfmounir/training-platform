640a3c54c74042ba0ae4b024aa3ac79d35012dc0
bfmounir <bfmounir@gmail.com>
2026-09-16 02:00:30 +0100

    Refactor supabase client initialization

--- supabase-config.js
+++ supabase-config.js
@@ -1,10 +1,11 @@
-const SUPABASE_URL = 
+const SUPABASE_URL =
 "https://vgbgvdbssiuhzeepexll.supabase.co";
 
-const SUPABASE_KEY = 
+const SUPABASE_KEY =
 "sb_publishable_8_ACYxPHx8ZYi5oA61x9rQ_IwNQossu";
 
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+window.supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
